f6839663b7341f435e2de10b3d1b1c9e5bdbbfcc
rpsobolewski <rpsobolewski@gmail.com>
2023-09-08 17:31:43 +0200

    added posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,56 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const roeElement = document.querySelector('.post');
+
+
+
+
+function addNewPost(post) {
+    const markup =
+        `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">
+                <div class="post-meta__icon">
+                    <img class="profile-pic"
+                        src="${post.author.image}"
+                        alt="${post.author.name}">
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt>
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#"
+                        data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up"
+                            aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1"
+                        class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div>
+        </div>
+    </div>`
+
+    roeElement.insertAdjacentHTML('beforeend', markup);
+}
+
+
+
+posts.forEach(post => {
+    addNewPost(post)
+});
